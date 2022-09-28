12786fe43c2843a9f1bb356c7b22b482992d12e0
EndlessDebugger <dortizchaves@outlook.com>
2022-09-28 17:19:10 -0500

    Creadted base files; filled requestqueue.h

---
+++ requestqueue.h
@@ -0,0 +1,52 @@
+#include <queue>
+#include <string>
+#include <exception>
+#include <stdlib.h>
+
+using std::string, std::queue, std::rand, std::to_string;
+
+struct Request{
+    string ip_in;
+    string ip_out;
+    int time;
+
+    Request(): ip_in(gen_ip()), ip_out(gen_ip()),time(gen_time()){};
+    Request(string in, string out, int t): ip_in(in), ip_out(out),time(t){};
+
+    string gen_ip(){
+        return to_string(rand()%256)+"."+to_string(rand()%256)+"."+to_string(rand()%256)+"."+to_string(rand()%256);
+    };
+
+    int gen_time(){ return rand(); };
+};
+
+
+class RequestQueue{
+    private:
+    queue<Request> holder;
+
+    public:
+
+    bool add(Request x){
+        try{
+            holder.push(x);
+        }
+        catch(std::exception e){
+            return false;
+        }
+        
+        return true;
+    }
+
+    Request pop(){
+        if(!empty()){
+            return holder.pop();
+        }
+        
+        throw "Queue is empty";
+    }
+
+    bool empty(){
+        return holder.empty();
+    }
+};
